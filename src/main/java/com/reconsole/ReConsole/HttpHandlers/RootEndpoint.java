d15851d1ac732e599640c923afa565db9eab2f0e
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2019-01-13 00:58:19 +0530

    So, the plugin actually works now \o/ The / endpoint is completely functional now. Also, registering a command without declaring in plugin.yml was crashing it.

--- src/main/java/com/reconsole/ReConsole/HttpHandlers/RootEndpoint.java
+++ src/main/java/com/reconsole/ReConsole/HttpHandlers/RootEndpoint.java
@@ -1,14 +1,26 @@
 package com.reconsole.ReConsole.HttpHandlers;
 
+import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.bukkit.Server;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class RootEndpoint implements HttpHandler {
+    private Server server;
+    public RootEndpoint(Server server1) { server = server1; }
+
     public void handle(HttpExchange exchange) throws IOException {
-        String res = "{\"code\": 200}";
+        // Build the JSON object.
+        JsonObject json = new JsonObject();
+        json.addProperty("code", 200);
+        json.addProperty("online", true);
+        json.addProperty("maxPlayers", server.getMaxPlayers());
+        json.addProperty("playersOnline", server.getOnlinePlayers().size());
+        json.addProperty("versionName", server.getVersion());
+        String res = json.toString();
         // Sending the response.
         exchange.sendResponseHeaders(200, res.length());
         OutputStream outputStream = exchange.getResponseBody();
--- src/main/java/com/reconsole/ReConsole/Main.java
+++ src/main/java/com/reconsole/ReConsole/Main.java
@@ -1,12 +1,11 @@
 package com.reconsole.ReConsole;
 
 // Plugin related imports.
-import com.reconsole.ReConsole.Commands.EasyUptime;
-import com.reconsole.ReConsole.HttpHandlers.RootEndpoint;
 import org.bukkit.plugin.java.JavaPlugin;
 
 // Handler classes.
 import com.reconsole.ReConsole.HttpHandlers.Login;
+import com.reconsole.ReConsole.HttpHandlers.RootEndpoint;
 
 // HTTP server related imports.
 import com.sun.net.httpserver.HttpServer;
@@ -14,22 +13,20 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Main extends JavaPlugin {
-    private long startTime = System.nanoTime();
     private HttpServer server;
 
     @Override
     public void onEnable () {
         // Start HTTP server.
         try {
-            this.server = HttpServer.create(new InetSocketAddress("localhost", 4200), 0);
+            this.server = HttpServer.create(new InetSocketAddress(4200), 0);
             this.server.setExecutor(null);
             this.server.createContext("/login", new Login());
-            this.server.createContext("/", new RootEndpoint());
-            System.out.println("HTTP server successfully listening on port 4200.");
+            this.server.createContext("/", new RootEndpoint(this.getServer()));
+            this.server.start();
+            System.out.println("[ReConsole] HTTP server successfully listening on port 4200.");
         } catch (IOException e) {
-            System.err.println("HTTP server failed to listen on port 4200!");
+            System.err.println("[ReConsole] HTTP server failed to listen on port 4200!");
         }
-        // Setup commands (this command is pretty much useless atm).
-        this.getCommand("easyUptime").setExecutor(new EasyUptime(startTime));
     }
 }
