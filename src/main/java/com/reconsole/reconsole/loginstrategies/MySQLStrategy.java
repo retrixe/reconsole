c7af5221d5378697a847934de6a10621363d55ad
AstelonM <banicamihai96@gmail.com>
2019-07-18 18:13:16 +0300

    Fixed sql strategies

--- src/main/java/com/reconsole/reconsole/loginstrategies/MySQLStrategy.java
+++ src/main/java/com/reconsole/reconsole/loginstrategies/MySQLStrategy.java
@@ -1,12 +1,44 @@
 package com.reconsole.reconsole.loginstrategies;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MySQLStrategy extends SQLStrategy {
 
-    public MySQLStrategy(JavaPlugin javaPlugin) throws SQLException {
-        super(javaPlugin, "mysql");
+    public MySQLStrategy(JavaPlugin plugin) throws SQLException {
+        super(plugin);
+    }
+
+    @Override
+    protected void initConnection(JavaPlugin plugin) throws SQLException {
+        ConfigurationSection section = plugin.getConfig().getConfigurationSection("mysql");
+        connection = DriverManager.getConnection(
+                section.getString("connection-url").replace("{df}", plugin.getDataFolder().getPath()),
+                section.getString("username"),
+                section.getString("password"));
+    }
+
+    @Override
+    public boolean register(String username, String hashedPass) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE STRCMP(username, ?) = 0 " +
+                "AND STRCMP(password, ?) = 0 LIMIT 1", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+            statement.setString(1, username);
+            statement.setString(2, hashedPass);
+            ResultSet result = statement.executeQuery();
+            if (result.first()) {
+                return false;
+            } else {
+                result.moveToInsertRow();
+                result.updateString("username", username);
+                result.updateString("password", hashedPass);
+                result.insertRow();
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
